19daaec62722087dba022187ee899f34acc5bb1a
Jay Allard <jay_allard@ultimatesoftware.com>
2018-03-04 00:05:46 -0500

    updated api objects

--- src/main/java/com/allardworks/workinator3/httpapi/Partition.java
+++ src/main/java/com/allardworks/workinator3/httpapi/Partition.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jaya on 3/3/18.
@@ -12,10 +14,28 @@ import java.time.LocalDateTime;
 @Document(collection = "Partitions")
 @Data
 public class Partition {
+    @Data
+    public static class Configuration{
+        private int maxIdleTimeSeconds;
+        private int maxWorkerCount;
+    }
+
+    @Data
+    public static class Status {
+        private boolean hasWork;
+        private LocalDateTime dueDate;
+        private int workerCount;
+        private final List<Worker> workers = new ArrayList<>();
+    }
+
+    @Data
+    public static class Worker {
+        private String id;
+        private String rule;
+    }
+
+
     private final String partitionKey;
-    private int maxIdleTimeSeconds;
-    private int maxWorkerCount;
-    private boolean hasWork;
-    private LocalDateTime dueDate;
-    private int wokerCount;
+    private final Configuration configuration;
+    private final Status status;
 }
